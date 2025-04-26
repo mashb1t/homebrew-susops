df2ab2fad8b4e0617aa2a727d47e7376430f3663
Manuel Schmid <manuel.schmid@odt.net>
2025-04-26 18:25:40 +0200

    susops 1.0.1

--- Casks/susops.rb
+++ Casks/susops.rb
@@ -1,6 +1,6 @@
 cask "susops" do
-  version "1.0.0"
-  sha256 "ddd9c2140c1a0bce02a598df27e0bf9b1f7c83db742bc6f89b35226c03bb3160"
+  version "1.0.1"
+  sha256 "23030b8c790e764b64999bac94b7ff57418d43443fe687d50542a9e5f3623b17"
 
   url "https://github.com/mashb1t/susops-mac/releases/download/v#{version}/SusOps.zip"
   name "SusOps"
