0b8ada8d1bf064e69af0296c7be4e819b8a72bbb
Manuel Schmid <manuel.schmid@odt.net>
2025-04-26 12:36:45 +0200

    feat: add Github action for automated update for cask and formula

--- Formula/susops.rb
+++ Formula/susops.rb
@@ -1,9 +1,11 @@
 class Susops < Formula
   desc "Lightweight CLI for website proxying and port forwarding"
   homepage "https://github.com/mashb1t/susops-cli"
-  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "399420ff3221f65553147739fec588f49831992bb2b455cd4ecaf0bc8d8873e3"
   license "MIT"
+
+  version "1.0.0"
+  sha256 "399420ff3221f65553147739fec588f49831992bb2b455cd4ecaf0bc8d8873e3"
+  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v#{version}.tar.gz"
 
   def install
     bin.install "susops.sh" => "susops"
@@ -11,7 +13,7 @@ class Susops < Formula
   end
 
   test do
-    assert_match "susops", shell_output("#{bin}/susops --help", 1)
-    assert_match "susops", shell_output("#{bin}/so --help", 1)
+    assert_match "Usage", shell_output("#{bin}/susops --help")
+    assert_match "Usage", shell_output("#{bin}/so --help")
   end
 end
