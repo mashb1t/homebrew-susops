cfce8563ba9c5f13b1cfb71fa009161d3fb32cc1
actions-user <actions@github.com>
2025-04-26 21:04:15 +0000

    susops 1.0.1

--- Formula/susops.rb
+++ Formula/susops.rb
@@ -1,9 +1,9 @@
 class Susops < Formula
   desc "Lightweight CLI for website proxying and port forwarding"
   homepage "https://github.com/mashb1t/susops-cli"
-  version "1.0.0"
-  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "399420ff3221f65553147739fec588f49831992bb2b455cd4ecaf0bc8d8873e3"
+  version "1.0.1"
+  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
 
   def install
     bin.install "susops.sh" => "susops"
