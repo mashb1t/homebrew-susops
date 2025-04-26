8d3e38a990f71aca5b06830e966906aefb91496a
Manuel Schmid <manuel.schmid@odt.net>
2025-04-26 21:26:17 +0200

    fix: change order or formula params

--- Formula/susops.rb
+++ Formula/susops.rb
@@ -1,9 +1,10 @@
 class Susops < Formula
-  desc "Lightweight CLI for website proxying and port forwarding"
-  homepage "https://github.com/mashb1t/susops-cli"
-  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v#{version}.tar.gz"
   version "1.0.0"
   sha256 "399420ff3221f65553147739fec588f49831992bb2b455cd4ecaf0bc8d8873e3"
+
+  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v#{version}.tar.gz"
+  desc "Lightweight CLI for website proxying and port forwarding"
+  homepage "https://github.com/mashb1t/susops-cli"
 
   def install
     bin.install "susops.sh" => "susops"
