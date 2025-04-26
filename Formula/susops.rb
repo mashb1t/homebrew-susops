c561235f4f97951347a782b76691231335d19806
Manuel Schmid <manuel.schmid@odt.net>
2025-04-26 17:57:02 +0200

    fix: remove new line

--- Formula/susops.rb
+++ Formula/susops.rb
@@ -2,7 +2,6 @@ class Susops < Formula
   desc "Lightweight CLI for website proxying and port forwarding"
   homepage "https://github.com/mashb1t/susops-cli"
   license "MIT"
-
   version "1.0.0"
   sha256 "399420ff3221f65553147739fec588f49831992bb2b455cd4ecaf0bc8d8873e3"
   url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v#{version}.tar.gz"
