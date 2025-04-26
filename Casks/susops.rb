b17eee444c338926ae0533d2671c0428d5478fe9
Manuel Schmid <9307310+mashb1t@users.noreply.github.com>
2025-04-26 23:58:26 +0200

    susops: update URL (#15)
    Co-authored-by: actions-user <actions@github.com>

--- Casks/susops.rb
+++ Casks/susops.rb
@@ -1,8 +1,8 @@
 cask "susops" do
-  version "1.0.0"
-  sha256 "ddd9c2140c1a0bce02a598df27e0bf9b1f7c83db742bc6f89b35226c03bb3160"
+  version "1.0.1"
+  sha256 "9710b997fe9a188e33fb6dd230a5444333552aaf5b438ca8950352410c8e9bc6"
 
-  url "https://github.com/mashb1t/susops-mac/releases/download/v1.0.0/SusOps.zip"
+  url "https://github.com/mashb1t/susops-mac/releases/download/v1.0.1/SusOps.zip"
   name "SusOps"
   desc "Menu bar app for website proxying and port forwarding"
   homepage "https://github.com/mashb1t/susops-mac"
