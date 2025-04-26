10c04c069b7be594eea285a496e4ae5ab36ae7b1
Manuel Schmid <manuel.schmid@odt.net>
2025-04-26 11:33:35 +0200

    initial commit

---
+++ Casks/susops.rb
@@ -0,0 +1,11 @@
+cask "susops" do
+  version "1.0.0"
+  sha256 "ddd9c2140c1a0bce02a598df27e0bf9b1f7c83db742bc6f89b35226c03bb3160"
+
+  url "https://github.com/mashb1t/susops-mac/releases/download/v#{version}/SusOps.zip"
+  name "SusOps"
+  desc "MacOS menu bar app for easy website proxying and port forwarding (SSH‑based SOCKS5 proxy + HTTP PAC server)"
+  homepage "https://github.com/mashb1t/susops-mac"
+
+  app "SusOps.app"
+end
---
+++ Formula/susops.rb
@@ -0,0 +1,17 @@
+class Susops < Formula
+  desc "Lightweight CLI tool for easy website proxying and port forwarding (SSH‑based SOCKS5 proxy + HTTP PAC server). No browser plugins needed."
+  homepage "https://github.com/mashb1t/susops-cli"
+  url "https://github.com/mashb1t/susops-cli/archive/v1.0.0.tar.gz"
+  sha256 "399420ff3221f65553147739fec588f49831992bb2b455cd4ecaf0bc8d8873e3"
+  license "MIT"
+
+  def install
+    bin.install "susops.sh" => "susops"
+    bin.install_symlink "susops" => "so"
+  end
+
+  test do
+    assert_match "susops", shell_output("#{bin}/susops --help", 1)
+    assert_match "susops", shell_output("#{bin}/so --help", 1)
+  end
+end
