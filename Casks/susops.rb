956cae2941917079c2047fdab0b10b6c33fcb41b
Manuel Schmid <manuel.schmid@odt.net>
2025-04-26 18:21:12 +0200

    fix: apply suggestions of brew style --fix

--- Casks/susops.rb
+++ Casks/susops.rb
@@ -1,10 +1,11 @@
 cask "susops" do
-  name "SusOps"
-  desc "MacOS menu bar app for website proxying and port forwarding"
-  homepage "https://github.com/mashb1t/susops-mac"
   version "1.0.0"
   sha256 "ddd9c2140c1a0bce02a598df27e0bf9b1f7c83db742bc6f89b35226c03bb3160"
+
   url "https://github.com/mashb1t/susops-mac/releases/download/v#{version}/SusOps.zip"
+  name "SusOps"
+  desc "Menu bar app for website proxying and port forwarding"
+  homepage "https://github.com/mashb1t/susops-mac"
 
   app "SusOps.app"
 end
--- Formula/susops.rb
+++ Formula/susops.rb
@@ -1,9 +1,9 @@
 class Susops < Formula
   desc "Lightweight CLI for website proxying and port forwarding"
   homepage "https://github.com/mashb1t/susops-cli"
+  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v#{version}.tar.gz"
   version "1.0.0"
   sha256 "399420ff3221f65553147739fec588f49831992bb2b455cd4ecaf0bc8d8873e3"
-  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v#{version}.tar.gz"
 
   def install
     bin.install "susops.sh" => "susops"
