edc94ce2bea6cb7ddf51b4db997d0d5d8477e2f0
Manuel Schmid <manuel.schmid@odt.net>
2025-04-26 21:33:50 +0200

    fix: use fixed version for fornula

--- Casks/susops.rb
+++ Casks/susops.rb
@@ -2,7 +2,7 @@ cask "susops" do
   version "1.0.0"
   sha256 "ddd9c2140c1a0bce02a598df27e0bf9b1f7c83db742bc6f89b35226c03bb3160"
 
-  url "https://github.com/mashb1t/susops-mac/releases/download/v#{version}/SusOps.zip"
+  url "https://github.com/mashb1t/susops-mac/releases/download/v1.0.0/SusOps.zip"
   name "SusOps"
   desc "Menu bar app for website proxying and port forwarding"
   homepage "https://github.com/mashb1t/susops-mac"
