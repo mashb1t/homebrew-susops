dffa5a9a389e705f39e1070b0176e5b59f07d63a
Manuel Schmid <manuel.schmid@odt.net>
2025-04-26 22:41:30 +0200

    fix: change order so PR message is always version

--- Formula/susops.rb
+++ Formula/susops.rb
@@ -1,8 +1,8 @@
 class Susops < Formula
   desc "Lightweight CLI for website proxying and port forwarding"
   homepage "https://github.com/mashb1t/susops-cli"
-  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v1.0.1.tar.gz"
   version "1.0.1"
+  url "https://github.com/mashb1t/susops-cli/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "70131258fbd65c30b67b2a73493163347734b58fbdeaf7631e20469a5a8007ed"
 
   def install
