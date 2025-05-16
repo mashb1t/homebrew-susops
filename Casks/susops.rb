90812bf5e3ca99ec3ff249c449965fba1baf7355
Manuel Schmid <9307310+mashb1t@users.noreply.github.com>
2025-05-16 22:04:53 +0200

    ci: bump susops-mac to 2.1.2 (#33)
    Co-authored-by: actions-user <actions@github.com>

--- Casks/susops.rb
+++ Casks/susops.rb
@@ -1,8 +1,8 @@
 cask "susops" do
-  version "2.1.1"
-  sha256 "2e96cf89a431c4f3bf00819d401e09cc7e7d23f1a619d02f569e72433197224e"
+  version "2.1.2"
+  sha256 "76ccda793375e21280d9c561b2439e61a0430f39eba830b4ef45195c27da716a"
 
-  url "https://github.com/mashb1t/susops-mac/releases/download/v2.1.1/SusOps.zip"
+  url "https://github.com/mashb1t/susops-mac/releases/download/v2.1.2/SusOps.zip"
   name "SusOps"
   desc "Menu bar app for website proxying and port forwarding"
   homepage "https://github.com/mashb1t/susops-mac"
